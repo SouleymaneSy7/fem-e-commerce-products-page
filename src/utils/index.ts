6b9de8a868f3a3cb070e2aa692c75869c71079bc
Souleymane Sy <souleymanesyservices@gmail.com>
2024-06-30 07:10:32 +0200

    config: add centralized imports for (images and svg)

---
+++ src/utils/index.ts
@@ -0,0 +1,44 @@
+// Images
+import avatar from "@/assets/images/image-avatar.png";
+
+import product_thumbnail_1 from "@/assets/images/image-product-1-thumbnail.jpg";
+import product_thumbnail_2 from "@/assets/images/image-product-2-thumbnail.jpg";
+import product_thumbnail_3 from "@/assets/images/image-product-3-thumbnail.jpg";
+import product_thumbnail_4 from "@/assets/images/image-product-4-thumbnail.jpg";
+
+import product_img_1 from "@/assets/images/image-product-1.jpg";
+import product_img_2 from "@/assets/images/image-product-2.jpg";
+import product_img_3 from "@/assets/images/image-product-3.jpg";
+import product_img_4 from "@/assets/images/image-product-4.jpg";
+
+import {
+  Logo,
+  Close,
+  Delete,
+  Minus,
+  Plus,
+  Previous,
+  Next,
+} from "@/components/icons/Icons.component";
+
+export {
+  avatar as avatarImg,
+  product_thumbnail_1,
+  product_thumbnail_2,
+  product_thumbnail_3,
+  product_thumbnail_4,
+  product_img_1,
+  product_img_2,
+  product_img_3,
+  product_img_4,
+};
+
+export {
+  Logo as LogoIcon,
+  Close as CloseIcon,
+  Delete as DeleteIcon,
+  Minus as MinusIcon,
+  Plus as PlusIcon,
+  Previous as PreviousIcon,
+  Next as NextIcon,
+};
