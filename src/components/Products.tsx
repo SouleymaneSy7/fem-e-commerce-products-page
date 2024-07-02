704c6f235a3d49e5300bbeba5d5fc9ea559cf7dc
Souleymane Sy <souleymanesyservices@gmail.com>
2024-07-03 01:21:52 +0200

    feat: add main products contents

---
+++ src/components/Products.tsx
@@ -0,0 +1,55 @@
+import Button from "./Button";
+import { mainContent } from "@/constants";
+import { formatNumber } from "@/utils/currencyFormat";
+import { CartIcon, MinusIcon, PlusIcon } from "@/utils";
+
+const productCounter = 14;
+
+const Products = () => {
+  const {
+    subTitle,
+    title,
+    description,
+    price,
+    reduction,
+    fullPrice,
+    mainButtonText,
+  } = mainContent;
+
+  return (
+    <section className="products">
+      <h2 className="product__sub-title">{subTitle}</h2>
+      <h1 className="product__title">{title}</h1>
+      <p className="product__description">{description}</p>
+      <div className="product__price">
+        <div>
+          <h3 className="product__price--reduct">{formatNumber(price)}</h3>
+          <div className="product__price--percent">
+            <span>{reduction}</span>
+          </div>
+        </div>
+
+        <p className="product__price--full">{formatNumber(fullPrice)}</p>
+      </div>
+
+      <div className="product__counter">
+        <Button customClassName={"product__counter--minus"}>
+          <MinusIcon />
+        </Button>
+
+        <p className="product__counter--count">{productCounter}</p>
+
+        <Button customClassName={"product__counter--plus"}>
+          <PlusIcon />
+        </Button>
+      </div>
+
+      <Button customClassName={"product--btn"}>
+        <CartIcon />
+        {mainButtonText}
+      </Button>
+    </section>
+  );
+};
+
+export default Products;
