40b78523946c6076a5f2e918617b8fd949a009b8
Souleymane Sy <souleymanesyservices@gmail.com>
2024-07-03 01:24:50 +0200

    feat: add main component

---
+++ src/components/Main.tsx
@@ -0,0 +1,13 @@
+import MobileSlider from "./MobileSlider";
+import Products from "./Products";
+
+const Main = () => {
+  return (
+    <main className="main | container">
+      <MobileSlider />
+      <Products />
+    </main>
+  );
+};
+
+export default Main;
