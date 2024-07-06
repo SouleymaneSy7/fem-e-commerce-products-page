63d23a0fcb16cb3609aa952bedc5819102b0e3e6
Souleymane Sy <souleymanesyservices@gmail.com>
2024-07-06 17:14:50 +0200

    feat: add the lightbox components to the main

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,13 @@
 import { Images } from "@/constants";
-import MobileCarouselSlider from "./MobileCarouselSlider";
 import Products from "./Products";
+import MobileCarouselSlider from "./MobileCarouselSlider";
+import LightBoxContainer from "./LightBoxContainer";
 
 const Main = () => {
   return (
     <main className="main | container">
       <MobileCarouselSlider sliderImages={Images} />
+      <LightBoxContainer productImages={Images} />
       <Products />
     </main>
   );
