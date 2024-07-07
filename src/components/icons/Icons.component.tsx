b4fb802d729ba4d08c975ee1d469cc1601c22c0c
Souleymane Sy <souleymanesyservices@gmail.com>
2024-07-07 23:49:57 +0200

    fix: fix the fill and stroke element in the svgs

--- src/components/icons/Icons.component.tsx
+++ src/components/icons/Icons.component.tsx
@@ -21,7 +21,7 @@ export const Cart = (props: SVGProps<SVGSVGElement>) => (
 );
 
 export const Close = (props: SVGProps<SVGSVGElement>) => (
-  <svg width="14" height="15" xmlns="http://www.w3.org/2000/svg" {...props}>
+  <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg" {...props}>
     <path
       fill="#69707D"
       fillRule="evenodd"
