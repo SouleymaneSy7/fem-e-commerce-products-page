0612a5ec668b51cd043a62971064bdb7f1eff2b7
Souleymane Sy <souleymanesyservices@gmail.com>
2024-07-08 07:26:54 +0200

    refactor: refactor the Cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,6 +1,11 @@
+import React from "react";
+import { createPortal } from "react-dom";
+
 import Button from "./Button";
 import { formatNumber } from "@/utils/currencyFormat";
 import { useProductCounter } from "@/contexts/ProductContext";
+
+const cartModalDOM = document.querySelector("#cart-modal-root");
 
 type CartPropsType = {
   isOpen: boolean;
@@ -30,38 +35,49 @@ const Cart = ({ isOpen, cartItemContent, cartEmpty }: CartPropsType) => {
   const totalPrice = cartPrice * productCounter;
 
   return (
-    <article className={`cart ${isOpen ? "active" : ""}`}>
-      <header className="cart__header">
-        <h3 className="cart__title">{cartTitle}</h3>
-      </header>
-      <main className="cart__main">
-        {productCounter > 0 ? (
-          <section>
-            <div className="cart__content">
-              <div className="cart__img">
-                <img src={cartImg} alt="Sneaker on top of others image" />
-              </div>
+    <React.Fragment>
+      {isOpen &&
+        createPortal(
+          <article className="cart">
+            <header className="cart__header">
+              <h3 className="cart__title">{cartTitle}</h3>
+            </header>
+            <main className="cart__main">
+              {productCounter > 0 ? (
+                <section>
+                  <div className="cart__content">
+                    <div className="cart__img">
+                      <img src={cartImg} alt="Sneaker on top of others image" />
+                    </div>
 
-              <div className="cart__content__text">
-                <p className="cart__content__text__title">{cartContentTitle}</p>
-                <p className="cart__content__text--price">
-                  {formatNumber(cartPrice)} * {productCounter}
-                  <span>{formatNumber(totalPrice)}</span>
-                </p>
-              </div>
+                    <div className="cart__content__text">
+                      <p className="cart__content__text__title">
+                        {cartContentTitle}
+                      </p>
+                      <p className="cart__content__text--price">
+                        {formatNumber(cartPrice)} * {productCounter}
+                        <span>{formatNumber(totalPrice)}</span>
+                      </p>
+                    </div>
 
-              <Button className="cart--icon" onClick={resetProductCounter}>
-                <CartDeleteIcon />
-              </Button>
-            </div>
+                    <Button
+                      className="cart--icon"
+                      onClick={resetProductCounter}
+                    >
+                      <CartDeleteIcon />
+                    </Button>
+                  </div>
 
-            <Button className="cart--btn">{cartButtonText}</Button>
-          </section>
-        ) : (
-          <p className="cart__empty">{cartEmpty}</p>
+                  <Button className="cart--btn">{cartButtonText}</Button>
+                </section>
+              ) : (
+                <p className="cart__empty">{cartEmpty}</p>
+              )}
+            </main>
+          </article>,
+          cartModalDOM!
         )}
-      </main>
-    </article>
+    </React.Fragment>
   );
 };
 
